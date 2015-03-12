bb800d5a4a6f1e1f8407f790d9792e4eccde7c41
Kyle Bulloch & kenna warsinske <student@epicodus.com>
2015-03-12 11:20:18 -0700

    Make one point array a property

--- src/Scrabbler.php
+++ src/Scrabbler.php
@@ -1,12 +1,12 @@
 <?php
     class Scrabbler
     {
+        private $one_point = array('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T');
 
         function calculateScore($input)
         {
-            $one_point = array('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T');
             $point_counter = 0;
-            if(in_array($input, $one_point)){
+            if(in_array($input, $this->one_point)){
                 $point_counter += 1;
             }
             return "Score: " . $point_counter;
