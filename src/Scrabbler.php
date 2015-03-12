25a07158e56a8933e85b143db3e1d683cd1c87f8
Kyle Bulloch & kenna warsinske <student@epicodus.com>
2015-03-12 11:25:52 -0700

    Add method to score a two point single character

--- src/Scrabbler.php
+++ src/Scrabbler.php
@@ -2,12 +2,16 @@
     class Scrabbler
     {
         private $one_point = array('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T');
+        private $two_point = array('D', 'G');
 
         function calculateScore($input)
         {
             $point_counter = 0;
             if(in_array($input, $this->one_point)){
                 $point_counter += 1;
+            }
+            if(in_array($input, $this->two_point)){
+                $point_counter += 2;
             }
             return "Score: " . $point_counter;
         }
